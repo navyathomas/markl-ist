0cdc9e2585bb1fc067afd6c328e96c8c3b4e7328
navyathomas <navyathankam15@gmail.com>
2018-12-12 13:09:46 +0530

    uploaded

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="./markstyle.css" rel="marksheet"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+    </head>
+    <body class="mypink" >
+       
+    </body>
+    <table class="table">
+        <tr>
+            <td>name</td>
+            <td><input type="text"/></td>
+        </tr>
+        <tr>
+            <td>register number</td>
+            <td><input type="number"/></td>
+        </tr>
+        <tr>
+            <td>branch</td>
+                    <td><select>
+                            <option value="computer science">computer science</option>
+                            <option value="civil">civil</option>
+                            <option value="mechanical">mechanical</option>
+                            <option value="electronics">electronics</option>
+                            <option value="electrical">electrical</option>
+                </select></td>
+        </tr>
+        <tr>
+            <td>college</td>
+            <td><input type="text"/></td>
+        </tr>
+        <tr>
+            <td>gender</td>
+            <td><input type="radio" value="male"/>male</td>
+            <td><input type="radio" value="female"/>female</td>
+        </tr>
+        <tr>
+            <td>sub1 name</td>           
+            <td><input type="text"/></td>
+        </tr>
+        <tr>
+            <td>sub1 mark obtained</td>
+            <td><input type="number"/></td>
+        </tr>
+        <tr>
+            <td>sub1 total mark</td>
+            <td><input type="number"/></td>
+        </tr>
+        <tr>
+            <td>sub2 name</td>
+            <td><input type="text"/></td>
+        </tr>
+        <tr>
+            <td>sub2 mark obtained</td>
+            <td><input type="number"/></td>
+        </tr>
+        <tr>
+            <td>sub2 total mark</td>
+            <td><input type="number"/></td>
+        </tr>
+        <tr>
+            <td>sub3 name</td>
+            <td><input type="text"/></td>
+        </tr>
+        <tr>
+            <td>sub3 mark obtained</td>
+            <td><input type="number"/></td>
+        </tr>
+        <tr>
+            <td>sub3 total mark</td>
+            <td><input type="number"/></td>
+        </tr>
+        <tr>
+            <td>sub4 name</td>
+            <td><input type="text"/></td>
+        </tr>
+        <tr>
+            <td>sub4 mark obtained</td>
+            <td><input type="number"/></td>
+        </tr>
+        <tr>
+            <td>sub4 total mark</td>
+            <td><input type="number"/></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input  class="btn btn-success" type="submit" value="submit"/></td>
+        </tr>
+    </table>
+</html>
